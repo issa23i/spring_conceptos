b512214602d1c8750622596b925a64798336614b
ipastorl <isabel.pastorlopez@emeal.nttdata.com>
2024-02-12 10:30:23 +0100

    Uso properties

---
+++ src/main/java/com/springsimplespasos/conceptosbasicos/usoproperties/ReadCommonProperties.java
@@ -0,0 +1,14 @@
+package com.springsimplespasos.conceptosbasicos.usoproperties;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ReadCommonProperties {
+    @Value("${app.base.url}")
+    private String baseUrl;
+
+    public String getBaseUrl() {
+        return baseUrl;
+    }
+}
