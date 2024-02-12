7dc6814b65fd0563e5a68c472b0c464ffef1d8b6
ipastorl <isabel.pastorlopez@emeal.nttdata.com>
2024-02-12 11:30:15 +0100

    Uso properties: Herencia

--- src/main/java/com/springsimplespasos/conceptosbasicos/usoproperties/PruebaProperties.java
+++ src/main/java/com/springsimplespasos/conceptosbasicos/usoproperties/PruebaProperties.java
@@ -1,5 +1,6 @@
 package com.springsimplespasos.conceptosbasicos.usoproperties;
 
+import com.springsimplespasos.conceptosbasicos.usoproperties.herencia.ReadBarProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -8,9 +9,20 @@ import org.springframework.stereotype.Component;
 public class PruebaProperties implements CommandLineRunner {
     @Autowired
     ReadCommonProperties commonProperties;
+    @Autowired
+    ReadFooProperties readFooProperties;
+    @Autowired
+    ReadBarProperties readBarProperties;
 
     @Override
     public void run(String... args) throws Exception {
         System.out.println("app.base.url: " + commonProperties.getBaseUrl());
+        System.out.println("=================================================");
+        System.out.println("app.foo.client: " + readFooProperties.getClientUrl());
+        System.out.println("app.foo.customer: " + readFooProperties.getCustomerUrl());
+        System.out.println("=================================================");
+        System.out.println("bar.name: " + readBarProperties.getName());
+        System.out.println("bar.url.client: " + readBarProperties.getUrl().getClient());
+        System.out.println("bar.url.customer: " + readBarProperties.getUrl().getCustomer());
     }
 }
